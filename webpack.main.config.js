82650ed43dc054c3e5b1070cc50846d1b70812b2
Azucenamc18azu <mc18azumc@gmail.com>
2025-04-07 11:40:41 -0700

    Agregando archivos de configuración de proyecto

---
+++ webpack.main.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+    ],
+  },
+  /**
+   * This is the main entry point for your application, it's the first file
+   * that runs in the main process.
+   */
+  entry: './src/main.js',
+  // Put your normal webpack config below here
+  module: {
+    rules: require('./webpack.rules'),
+  },
+};
